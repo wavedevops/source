722605230fad7f850cb370516b054d42567a7670
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-26 18:33:47 +0530

    automated commit #3485

--- infra-create-shell-script/rhel.sh
+++ infra-create-shell-script/rhel.sh
@@ -15,8 +15,7 @@ create_ec2() {
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --region ${REGION} --change-batch file:///tmp/record.json | jq
+  echo "EC2 instance created with Private IP: ${PRIVATE_IP}"
 }
 
 ## Main Program
@@ -28,14 +27,4 @@ fi
 
 SGID=$(aws ec2 describe-security-groups --region ${REGION} --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
-  echo "Given Security Group does not exist"
-  exit 1
-fi
-
-COMPONENT="${1}"
-if [ -z "${COMPONENT}" ]; then
-  echo "Input component name is needed"
-  exit 1
-fi
-
-create_ec2
+  echo "Given Security Group does not exist
