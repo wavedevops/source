b6f58253e1a3a09cee7d6616a5ccc2b70775f5a0
Chowdary-Hari <chandrahari321@outlook.com>
2024-08-26 18:40:40 +0530

    automated commit #3486

--- infra-create-shell-script/rhel.sh
+++ infra-create-shell-script/rhel.sh
@@ -6,25 +6,41 @@ REGION="us-east-1"  # Specify your desired AWS region here (e.g., us-east-1, us-
 #############################
 
 create_ec2() {
+  echo "Creating EC2 instance..."
   PRIVATE_IP=$(aws ec2 run-instances \
       --region ${REGION} \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
-      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  echo "EC2 instance created with Private IP: ${PRIVATE_IP}"
+  echo "EC2 instance created with IP: ${PRIVATE_IP}"
 }
 
 ## Main Program
+
+# Find the AMI ID based on a specific name filter
 AMI_ID=$(aws ec2 describe-images --region ${REGION} --filters "Name=name,Values=RHEL-9-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1
 fi
 
-SGID=$(aws ec2 describe-security-groups --region ${REGION} --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+# Retrieve the Security Group ID based on the security group name
+SGID=$(aws ec2 describe-security-groups --region ${REGION} --filters "Name=group-name,Values=${SG_NAME}" | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
-  echo "Given Security Group does not exist
+  echo "Given Security Group does not exist"
+  exit 1
+fi
+
+# Validate the input for the component name
+COMPONENT="${1}"
+if [ -z "${COMPONENT}" ]; then
+  echo "Input component name is needed"
+  exit 1
+fi
+
+# Call the create_ec2 function to create the EC2 instance
+create_ec2
