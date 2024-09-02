a785f52f5ed104699ffccfc6a7ecd440b7ffb26c
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-02 18:39:21 +0530

    automated commit #3608

--- iam/data.tf
+++
@@ -1,5 +0,0 @@
-# Retrieve the password from AWS Systems Manager Parameter Store
-data "aws_ssm_parameter" "hari_password" {
-  name            = "iam"  # Replace with your actual parameter name
-  with_decryption = true                    # Decrypts the SecureString value
-}
--- iam/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "common_tags" {
-  type = map(string)
-}
