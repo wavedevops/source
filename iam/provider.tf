1eb0609c2d6ccfeaf4129872a0cc7911260810af
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-02 18:25:37 +0530

    automated commit #3597

--- iam/provider.tf
+++ iam/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "chowdary-hari"
+    key    = "iam/iam/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
--- iam/state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "chowdary-hari"
-    key    = "iam/iam/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
