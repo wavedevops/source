6b3e11ce332eb7d8c78e5dcb6a5bc11ddeee4988
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-02 18:45:54 +0530

    automated commit #3612

--- iam/iam.tf
+++ iam/iam.tf
@@ -19,12 +19,3 @@ resource "aws_iam_user_policy_attachment" "user_hari_admin_policy" {
 output "user_name" {
   value = aws_iam_user.user_hari.name
 }
-│ Error: Value for unconfigurable attribute
-│
-│   with aws_iam_user_login_profile.user_hari_profile,
-│   on iam.tf line 9, in resource "aws_iam_user_login_profile" "user_hari_profile":
-│    9:   password            = "@123Admin"
-│
-│ Can't configure a value for "password": its value will be decided automatically based on the result
-│ of applying this configuration.
-╵
