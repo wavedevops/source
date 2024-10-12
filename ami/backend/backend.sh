0f42c862d00339edcd2c674334b19767399567e7
Chowdary-Hari <chandrahari321@outlook.com>
2024-10-12 09:18:52 +0530

    automated commit #210

--- ami/backend/backend.sh
+++ ami/backend/backend.sh
@@ -5,4 +5,4 @@
 
  dnf install ansible -y
  pip3.9 install botocore boto3
- ansible-pull -i localhost, -U https://github.com/Chowdary-Hari/expense-ansible-tf.git main.yml -e component=$component -e env=$env
+ ansible-pull -i localhost, -U https://github.com/Chowdary-Hari/expense-ansible-tf.git main.yaml -e component=$component -e env=$env
