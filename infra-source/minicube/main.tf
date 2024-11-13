726086c9dd11ddb73aba5e7a55ac2869222fcb04
Chowdary-Hari <chandrahari321@outlook.com>
2024-11-13 21:20:00 +0530

    automated commit #651

--- infra-source/minicube/main.tf
+++ infra-source/minicube/main.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Terraform Backend Configuration (S3 for State Storage)
+terraform {
+  backend "s3" {
+    bucket = "chowdary-hari"                 # S3 bucket name for storing Terraform state
+    key    = "infra-source/minicube"      # State file path inside the S3 bucket
+    region = "us-east-1"                     # AWS region where the S3 bucket is located
+  }
+}
+
+
 variable "subnets" {
   default = {
     us-east-1b = "subnet-084d096cd8e354551",
